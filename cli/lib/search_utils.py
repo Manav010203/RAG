87ff7b9aeb062c2a0d442ac609c7a7e42a449c9e
Manav010203 <manav01022003@gmail.com>
2026-01-16 21:41:11 +0530

    updated

--- cli/lib/search_utils.py
+++ cli/lib/search_utils.py
@@ -1,19 +1,21 @@
 import json
 import os
 
-
 DEFAULT_SEARCH_LIMIT = 5
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 DATA_PATH = os.path.join(PROJECT_ROOT, "data", "movies.json")
-STOP_WORDS_PATH = os.path.join(PROJECT_ROOT,"data","stopwords.txt")
+STOPWORDS_PATH = os.path.join(PROJECT_ROOT, "data", "stopwords.txt")
+
+CACHE_DIR = os.path.join(PROJECT_ROOT, "cache")
 
 
 def load_movies() -> list[dict]:
     with open(DATA_PATH, "r") as f:
         data = json.load(f)
     return data["movies"]
-def load_Stop_words():
-    with open(STOP_WORDS_PATH,"r") as f:
-        data = f.read().splitlines()
-    return data
+
+
+def load_stopwords() -> list[str]:
+    with open(STOPWORDS_PATH, "r") as f:
+        return f.read().splitlines()
